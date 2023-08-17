d18d3dd7c5027ea137745a75f23c31d6c47849f5
lcsilva76 <lsilva@fiap.com.br>
2023-08-17 11:15:39 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
       <h1>Olá Mundo!!!</h1>
       <p>Vamos lá pessoal!!!</p>
+      <p>Mais um teste!!!</p>
       <p>Mais um teste!!!</p>
     </>
   )
